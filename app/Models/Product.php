13a6abecaecf88f9d14d8c4b3db80fc052def86e
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-02-19 07:50:01 +0000

    add column user_id to belongto for realtionship between user and product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -42,7 +42,7 @@ class Product extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class,'user_id')->withDefault();
     }
 
     public function favorites()
