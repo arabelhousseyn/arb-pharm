f704b59e2369bd2e5cd255a314746b295970d00a
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-02-19 07:57:41 +0000

    add user_id to user belongsto relationship between requestEstimate and user

--- app/Models/RequestEstimate.php
+++ app/Models/RequestEstimate.php
@@ -43,7 +43,7 @@ class RequestEstimate extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class,'user_id')->withDefault();
     }
 
     public function images()
